7e72f039d7ff21df2bbb94ac141c9d0ab64280d3
azizys <36770253+azizys@users.noreply.github.com>
2019-01-25 21:55:13 -0500

    player prefab

---
+++ Tendrils GGJ/Assets/Editor/EditorRadius.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Character))]
+public class NewBehaviourScript : Editor {
+
+    // public variables
+
+    // private variables
+
+
+    void OnSceneGUI() {
+
+        Character ch = (Character)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(ch.transform.position, Vector3.forward, Vector3.up, 360, ch.groundCheckRadius);
+    }
+}
