62d5f8bfcebf9335ac14d3077d0e26a355edbd99
Mike Chong <mcchong@uwaterloo.ca>
2019-01-26 16:39:05 -0500

    Added some descriptive errors to the csv parsing

--- Tendrils GGJ/Assets/Scripts/Load_Map.cs
+++ Tendrils GGJ/Assets/Scripts/Load_Map.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,28 +9,40 @@ public static class LoadMap {
     {
         List<PlanetParam> planets = new List<PlanetParam>();
 
-        TextAsset mapData = Resources.Load("map") as TextAsset;
-        List<string> planetData = new List<string>(mapData.text.Split('\n'));
-        planetData.RemoveAt(0); //Remove header line
-        foreach(string planetLine in planetData) {
-            string[] pp = planetLine.Split(',');
-            int x = int.Parse(pp[0]);
-            int y = int.Parse(pp[1]);
-            int scale = int.Parse(pp[2]);
-            int grav = int.Parse(pp[3]);
-            string spriteImage = pp[4];
+        try
+        {
+            TextAsset mapData = Resources.Load("map") as TextAsset;
+            List<string> planetData = new List<string>(mapData.text.Split('\n'));
+            planetData.RemoveAt(0); //Remove header line
+            foreach(string planetLine in planetData) {
+                string[] pp = planetLine.Split(',');
+                int x = int.Parse(pp[0]);
+                int y = int.Parse(pp[1]);
+                int scale = int.Parse(pp[2]);
+                int grav = int.Parse(pp[3]);
+                string spriteImage = pp[4];
 
-            // Rotation
-            float rot = newPlanetRotation();
+                // Rotation
+                float rot = newPlanetRotation();
 
-            PlanetParam p =  new PlanetParam(x, y, scale, grav, rot, spriteImage);
-            planets.Add(p);
+                PlanetParam p =  new PlanetParam(x, y, scale, grav, rot, spriteImage);
+                planets.Add(p);
+            }
+        } catch (NullReferenceException e) {
+            Debug.LogError("Can't find the csv file!");
+            throw e;
+        } catch (FormatException e) {
+            Debug.LogError("Can't parse the file, make sure your numbers are formatted correctly");
+            throw e;
+        } catch (IndexOutOfRangeException e) {
+            Debug.LogError("A planet didn't have enough params!");
+            throw e;
         }
 
         return planets;
     }
 
     private static float newPlanetRotation() {
-        return Random.Range(0.01f, 0.05f);
+        return UnityEngine.Random.Range(0.01f, 0.05f);
     }
 }
