ad0fc81ef2a14699bfe21b1cf98c51cd65e3deba
Mike Chong <mcchong@uwaterloo.ca>
2019-01-26 14:54:40 -0500

    More sensible map size

--- Tendrils GGJ/Assets/Scripts/Map_Gen.cs
+++ Tendrils GGJ/Assets/Scripts/Map_Gen.cs
@@ -23,15 +23,17 @@ public class Map_Gen : MonoBehaviour
     {
         for (int i = 0; i < 10; i++) {
             // Scale, Position
-            int x = Random.Range(-1000, 1000);
-            int y = Random.Range(-1000, 1000);
+            int x = Random.Range(-300, 300);
+            int y = Random.Range(-300, 300);
             Vector3 position = new Vector3(x, y, 0);
             int scale = Random.Range(6, 24);
             // Grav strength
             int grav = Random.Range(-98, -49);
+            // Rotation
+            int rot = Random.Range(1, 10);
 
             Planet newPlanet = (Planet) Instantiate(planet);
-            newPlanet.InitPlanet(position, scale, grav, 3);
+            newPlanet.InitPlanet(position, scale, grav, rot);
         }
     }
 
