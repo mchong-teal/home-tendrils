c2052edece46aecc6230df18ffc3b2033a6bee40
Robert <robert.blank.wardo@gmail.com>
2019-01-26 09:53:35 -0500

    jet fuel is working
    just need the physics part

--- Tendrils GGJ/Assets/Scripts/Fuel_System.cs
+++ Tendrils GGJ/Assets/Scripts/Fuel_System.cs
@@ -7,24 +7,28 @@ using UnityEngine.UI;
 public class Fuel_System : MonoBehaviour
 {
     public bool JetOn;
-    public bool delay;
-    public bool isRunning;
+    public float delay;
+    public float maxDelay;
     public float fuel;
     public float maxFuel;
     public Slider fuelslider;
-
+    Rigidbody2D playerRigidbody;
+    public float jetForce;
     // Start is called before the first frame update
     void Start()
     {
+
         JetOn = false;
-        isRunning = false;
-        delay = true;
+        maxDelay = 3.0f;
+        delay = maxDelay;
         maxFuel = 100.0f;
         fuel = maxFuel;
         fuelslider.maxValue = maxFuel;
         fuelslider.value = fuel;
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        jetForce = 2.0f;
 
-}
+    }
 
     // Update is called once per frame
     void Update()
@@ -33,13 +37,10 @@ public class Fuel_System : MonoBehaviour
       
         if (JetOn == false) //recharge for fuel
         {
-            if (isRunning == false)
-            {
-                StartCoroutine(FuelRecharge1());
-                
-
-            
-             }
+            delay -= Time.deltaTime;
+            if (delay <= 0.0f) {
+                fuel = fuel + 2.0f;
+            }
         }
         if (fuel>maxFuel)
         {
@@ -51,10 +52,10 @@ public class Fuel_System : MonoBehaviour
         }
         if (Input.GetButton("Jump") & fuel > 0.0f ){
             JetOn = true;
-            isRunning = true;
-            delay = false;
-            fuel = fuel - 0.16f;
-            
+            delay = maxDelay;
+            fuel = fuel - 0.5f;
+            playerRigidbody.AddForce(transform.right *jetForce );
+
         }
         else
         {
@@ -64,13 +65,5 @@ public class Fuel_System : MonoBehaviour
 
    
 
-    IEnumerator FuelRecharge1() //needed to use this to add a delay to the recharge
-    {
-        isRunning = true;
-            yield return new WaitForSeconds(3.0f);
-        delay = true;
-       
-
-
-    }
+    
 }
