54702a297b8737aa31c699151d63f8c750bb254d
Robert <robert.blank.wardo@gmail.com>
2019-01-26 19:35:48 -0500

    working on ui
    added text for tips/etc.

---
+++ Tendrils GGJ/Assets/Scripts/EventSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EventSystem : MonoBehaviour
+{
+    public TextMeshProUGUI eventText;
+    public bool textOn;
+    public float textTime;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textOn == true) { 
+        textTime -= Time.deltaTime;
+    }
+        if (textTime <= 0.0f)
+        {
+            eventText.text = "";
+            textOn = false;
+        }
+
+
+    }
+    public void Changetext()
+    {
+        textTime = 5.0f;
+        textOn = true;
+    }
+}
