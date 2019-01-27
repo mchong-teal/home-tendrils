a98db913e008e82afa96fd98531efb3a4c84d246
Mike Chong <mcchong@uwaterloo.ca>
2019-01-27 00:39:39 -0500

    abandoning

--- Tendrils GGJ/Assets/Scripts/Planet.cs
+++ Tendrils GGJ/Assets/Scripts/Planet.cs
@@ -30,6 +30,7 @@ public class Planet: MonoBehaviour
     // Refs to Unity components
     public AI_Controller npcPrefab;
     PointEffector2D gravityEffector;
+    CircleCollider2D circleCollider;
     AI_Controller npc;
 
     // Creates a Planet
@@ -37,12 +38,19 @@ public class Planet: MonoBehaviour
     {
         this.planetIdx = idx;
         this.transform.position = new Vector3(param.x, param.y, 0);
-        this.transform.localScale = new Vector3(Constants.PLAYER_SCALE * param.size, Constants.PLAYER_SCALE * param.size, 0);
+        this.circleCollider = this.GetComponent<CircleCollider2D>();
+        Vector3 planetSize = new Vector3(10, 10, 0);
+        transform.localScale = planetSize;
+
 
         // Gravity
         this.gravityEffector = GetComponentInChildren<PointEffector2D>();
         this.gravityEffector.forceMagnitude = param.gravity;
         this.rotation = param.rotation;
+
+        SpriteRenderer sr = this.GetComponent<SpriteRenderer>();
+        sr.sprite = Resources.Load<Sprite>("Planets/Earthlike");
+        circleCollider.radius = 6;
 
         // NPCs
         npc = (AI_Controller) Instantiate(npcPrefab, this.transform.position + (Vector3.up * this.transform.localScale.x), Quaternion.identity);
@@ -61,7 +69,7 @@ public class Planet: MonoBehaviour
         transform.Rotate(0, 0, this.rotation);
         int rand = Random.Range(0, 1);
         if (rand == 0) { rand = -1; }
-	npc.PlanetPosition(this.transform.position, this.transform.localScale.x * this.GetComponent<CircleCollider2D>().radius, this.rotation * (float)rand);
+	npc.PlanetPosition(this.transform.position, this.transform.localScale.x * this.circleCollider.radius, this.rotation * (float)rand);
     }
 
 }
